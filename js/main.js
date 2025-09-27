89a834db00c5aec4217a50216ed3b13dc16e29fa
lg047 <88402180+lg047@users.noreply.github.com>
2025-09-27 16:01:10 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// Highlight active nav link
+(function(){
+  const path = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('.nav a').forEach(a=>{
+    const href = a.getAttribute('href');
+    if ((path === '' && href === 'index.html') || href === path) {
+      a.classList.add('active');
+    }
+    // Open external links in new tab
+    if (a.host !== location.host) a.setAttribute('target','_blank');
+  });
+})();
 
